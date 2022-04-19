fbe362c36a3db369bc1e212adbcb23641c9bf5d9
teal52u <mys.alter10@gmail.com>
2022-04-19 09:12:04 +0900

    edit script

--- scripts/joy_to_twist.py
+++ scripts/joy_to_twist.py
@@ -8,4 +8,19 @@ def joy_to_twist(msg , twist_pub):
     L_horizontal = msg.axes[0]
     L_vertical = msg.axes[1]
     circle = msg.buttons[13]
-    velocity = [L_horizontal*(1+circle),]
+    velocity = [L_horizontal*(1+circle),L_vertical*(1+circle)]
+
+    t = Twist()
+
+    t.angular.z, t.linear.x = velocity
+
+    twist_pub.publish(t)
+
+
+if __name__ == '__main__':
+    rospy.init_node('joy_to_twist')
+    twist_pub = rospy.publisher('turtle1/cmd_vel',Twist,queue_size=1)
+
+    rospy.Subscriber('/joy',Joy,joy_to_twist,twist_pub)
+
+    rospy.spin()
